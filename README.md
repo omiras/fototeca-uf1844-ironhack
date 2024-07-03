f79ae74517355fc18a6a65d81587e4b50cbc5245
omiras <oscar.miras@gmail.com>
2024-07-03 20:50:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Fototeca
+
+## Setup
+
+1. Instalar dependencias
+
+`npm install`
+
+2. Ejecutar el proyecto
+
+`npm run dev`
---
+++ app.js
@@ -0,0 +1,48 @@
+// importar módulos de terceros
+const express = require('express');
+const morgan = require('morgan');
+
+// creamos una instancia del servidor Express
+const app = express();
+
+// Tenemos que usar un nuevo middleware para indicar a Express que queremos procesar peticiones de tipo POST
+app.use(express.urlencoded({ extended: true }));
+
+// Base de datos de imágenes
+const images = [];
+
+// Especificar a Express que quiero usar EJS como motor de plantillas
+app.set('view engine', 'ejs');
+
+// Usamos el middleware morgan para loguear las peticiones del cliente
+app.use(morgan('tiny'));
+
+// Cuando nos hagan una petición GET a '/' renderizamos la home.ejs
+app.get('/', (req, res) => {
+
+    // 2. Usar en el home.ejs el forEach para iterar por todas las imágenes de la variable 'images'. Mostrar de momento solo el título 
+    res.render('home');
+});
+
+// Cuando nos hagan una petición GET a '/add-image-form' renderizamos 
+app.get('/add-image-form', (req, res) => {
+    res.render('form');
+});
+
+// Cuando nos hagan una petición POST a '/add-image-form' tenemos que recibir los datos del formulario y actualizar nuestra "base de datos"
+app.post('/add-image-form', (req, res) => {
+    // todos los datos vienen en req.body
+    console.log(req.body);
+
+    // 1. Actualizar el array 'images' con la información de req.body
+
+    // 3. Añadir los otros campos del formulario y sus validaciones 
+    res.send('Datos recibidos');
+});
+
+// en el futuro es normal que tengamos endpoints como
+// app.get('/edit-image-form')
+
+app.listen(3000, (req, res) => {
+    console.log("Servidor escuchando correctamente en el puerto 3000.")
+});
